517e3bef5605e72d5aa98a6a69e1812a9091f47f
leixiaoyue <leixiaoyue@oppo.com>
2016-04-06 19:32:47 +0800

    [SparkleBike][INIT][初始化提交] 分析：[分析省略] 方案：[方案省略] 风险：[暂无风险]

---
+++ 2-Coding/0-Android/SparkleBike/app/src/main/java/cn/geekiot/sparklebike/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.geekiot.sparklebike;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 80119424 on 2016/4/6.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
